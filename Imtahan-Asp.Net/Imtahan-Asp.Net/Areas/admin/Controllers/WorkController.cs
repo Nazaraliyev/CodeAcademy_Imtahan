8fcf0c6158fd40ae6ec42bb9ec2e8e690bec8899
Nazaraliyev <nazaralliyev@hotmail.com>
2022-01-28 19:35:44 +0400

    work crud is ready

--- Imtahan-Asp.Net/Imtahan-Asp.Net/Areas/admin/Controllers/WorkController.cs
+++ Imtahan-Asp.Net/Imtahan-Asp.Net/Areas/admin/Controllers/WorkController.cs
@@ -78,6 +78,119 @@ namespace Imtahan_Asp.Net.Areas.admin.Controllers
         }
 
 
+        public async Task<IActionResult> Update(int? Id)
+        {
+            if (Id == null)
+            {
+                return NotFound();
+            }
+
+            if (await _context.works.FindAsync(Id) == null)
+            {
+                return NotFound();
+            }
+
+            Work work = await _context.works.FindAsync(Id);
+            VmWorkUpdate model = new VmWorkUpdate()
+            {
+                Id = work.Id,
+                Tittle = work.Tittle,
+                Text=work.Text,
+                Photo = work.Photo
+            };
+
+            return View(model);
+
+        }
+
+
+        [HttpPost]
+        public IActionResult Update(VmWorkUpdate model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            if (model.PhotoFile != null)
+            {
+                if (model.PhotoFile.ContentType == "image/jpeg" || model.PhotoFile.ContentType == "image/png")
+                {
+                    if (model.PhotoFile.Length > 3 * 1024 * 1024)
+                    {
+                        ModelState.AddModelError("", "You can only upload Image until 3mb");
+                        return View(model);
+                    }
+
+
+                    string oldImage = Path.Combine("wwwroot", "assets/img/team", model.Photo);
+                    if (System.IO.File.Exists(oldImage))
+                    {
+                        System.IO.File.Delete(oldImage);
+                    }
+
+
+                    string fileName = Guid.NewGuid() + "-" + model.PhotoFile.FileName;
+                    string filePath = Path.Combine("wwwroot", "assets/img/work", fileName);
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        model.PhotoFile.CopyTo(stream);
+                    }
+
+                    model.Photo = fileName;
+                }
+                else
+                {
+                    ModelState.AddModelError("", "You can only upload image file");
+                    return View(model);
+                }
+            }
+
+            Work work = new Work()
+            {
+                Id = model.Id,
+                Tittle = model.Tittle,
+                Text = model.Text,
+                Photo = model.Photo,
+            };
+
+            _context.works.Update(work);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
+
+
+        public async Task<IActionResult> Delete(int? Id)
+        {
+            if (Id == null)
+            {
+                return NotFound();
+            }
+
+            if (await _context.works.FindAsync(Id) == null)
+            {
+                return NotFound();
+            }
+
+            Work willdeleteweork = await _context.works.FindAsync(Id);
+
+            string Image = Path.Combine("wwwroot", "assets/img/work", willdeleteweork.Photo);
+            if (System.IO.File.Exists(Image))
+            {
+                System.IO.File.Delete(Image);
+            }
+
+
+            _context.works.Remove(willdeleteweork);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
 
     }
 }
---
+++ Imtahan-Asp.Net/Imtahan-Asp.Net/ViewModel/VmWorkkUpdate.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Imtahan_Asp.Net.ViewModel
+{
+    public class VmWorkUpdate
+    {
+        public int Id { get; set; }
+
+
+        [MaxLength(250)]
+        public string Photo { get; set; }
+
+
+        [MaxLength(50), Required]
+        public string Tittle { get; set; }
+
+
+        [MaxLength(300), Required]
+        public string Text { get; set; }
+
+
+        public IFormFile PhotoFile { get; set; }
+    }
+}
